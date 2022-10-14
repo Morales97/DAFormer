7c0c7ff344ba27cd2e23937e7779722588792ff5
Morales97 <danimoralesbrotons@gmail.com>
2022-10-14 12:42:54 +0200

    Update subsample_cityscapes.py

--- tools/convert_datasets/subsample_cityscapes.py
+++ tools/convert_datasets/subsample_cityscapes.py
@@ -12,23 +12,6 @@ from PIL import Image
 import pdb
 import shutil
 
-def convert_json_to_label(json_file):
-    label_file = json_file.replace('_polygons.json', '_labelTrainIds.png')
-    json2labelImg(json_file, label_file, 'trainIds')
-
-    if 'train/' in json_file:
-        pil_label = Image.open(label_file)
-        label = np.asarray(pil_label)
-        sample_class_stats = {}
-        for c in range(19):
-            n = int(np.sum(label == c))
-            if n > 0:
-                sample_class_stats[int(c)] = n
-        sample_class_stats['file'] = label_file
-        return sample_class_stats
-    else:
-        return None
-
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -36,34 +19,8 @@ def parse_args():
     parser.add_argument('cityscapes_path', help='cityscapes data path')
     parser.add_argument('--gt-dir', default='gtFine', type=str)
     parser.add_argument('-o', '--out-dir', help='output path')
-    parser.add_argument(
-        '--nproc', default=1, type=int, help='number of process')
     args = parser.parse_args()
     return args
-
-
-def save_class_stats(out_dir, sample_class_stats):
-    sample_class_stats = [e for e in sample_class_stats if e is not None]
-    with open(osp.join(out_dir, 'sample_class_stats.json'), 'w') as of:
-        json.dump(sample_class_stats, of, indent=2)
-
-    sample_class_stats_dict = {}
-    for stats in sample_class_stats:
-        f = stats.pop('file')
-        sample_class_stats_dict[f] = stats
-    with open(osp.join(out_dir, 'sample_class_stats_dict.json'), 'w') as of:
-        json.dump(sample_class_stats_dict, of, indent=2)
-
-    samples_with_class = {}
-    for file, stats in sample_class_stats_dict.items():
-        for c, n in stats.items():
-            if c not in samples_with_class:
-                samples_with_class[c] = [(file, n)]
-            else:
-                samples_with_class[c].append((file, n))
-    with open(osp.join(out_dir, 'samples_with_class.json'), 'w') as of:
-        json.dump(samples_with_class, of, indent=2)
-
 
 def main():
     args = parse_args()
@@ -86,48 +43,13 @@ def main():
     idxs = np.random.permutation(idxs)
     labeled_files = (np.array(files)[idxs[:n_labeled_samples]]).tolist()
     
-    # copy selected images in new folder
+    # copy selected images and corresponding labels in new folder
     for f in labeled_files:
         shutil.copy(f, new_cityscapes_path_img)
         f_split = f.split(os.sep)
         label = osp.join(gt_dir, f_split[-3], f_split[-2], f_split[-1][:-15] + 'gtFine_labelTrainIds.png') # -3: train, -2: <city>, -1: <sample>
         shutil.copy(label, new_cityscapes_path_lbl)
-        pdb.set_trace()
 
-    # copy labels
-    labels = []
-    for f in mmcv.scandir(osp.join(cityscapes_path, 'gtFine/train'), 'labelTrainIds.png', recursive=True):
-        files.append(osp.join(cityscapes_path, 'leftImg8bit/train', f))
-
-    for f in labeled_files:
-        f_split = f.split(os.sep)
-        label = osp.join(gt_dir, f_split[-3], f_split[-2], f_split[-1][:-15] + 'labelTrainIds.png') # -3: train, -2: <city>, -1: <sample>
-        shutil.copy(label, new_cityscapes_path_lbl)
-
-
-    only_postprocessing = False
-    if not only_postprocessing:
-        if args.nproc > 1:
-            sample_class_stats = mmcv.track_parallel_progress(
-                convert_json_to_label, poly_files, args.nproc)
-        else:
-            sample_class_stats = mmcv.track_progress(convert_json_to_label,
-                                                     poly_files)
-    else:
-        with open(osp.join(out_dir, 'sample_class_stats.json'), 'r') as of:
-            sample_class_stats = json.load(of)
-
-    save_class_stats(out_dir, sample_class_stats)
-
-    split_names = ['train', 'val', 'test']
-
-    for split in split_names:
-        filenames = []
-        for poly in mmcv.scandir(
-                osp.join(gt_dir, split), '_polygons.json', recursive=True):
-            filenames.append(poly.replace('_gtFine_polygons.json', ''))
-        with open(osp.join(out_dir, f'{split}.txt'), 'w') as f:
-            f.writelines(f + '\n' for f in filenames)
 
 
 if __name__ == '__main__':
